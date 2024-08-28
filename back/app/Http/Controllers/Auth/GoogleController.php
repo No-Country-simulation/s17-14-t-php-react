b118422056dd7a040ef949d1bb633316282e987b
Carlos <fajardobeto72@gmail.com>
2024-08-28 15:40:02 -0500

    feat

--- back/app/Http/Controllers/Auth/GoogleController.php
+++ back/app/Http/Controllers/Auth/GoogleController.php
@@ -4,8 +4,46 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\GoogleUser;
+use Illuminate\Support\Facades\Validator;
 
 class GoogleController extends Controller
 {
-    //
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try {
+            $googleUser = Socialite::driver('google')->user();
+
+            // Busca el usuario en la base de datos
+            $user = GoogleUser::where('google_id', $googleUser->id)->first();
+
+            if ($user) {
+                // Inicia sesión con el usuario existente
+                Auth::login($user);
+            } else {
+                // Crea un nuevo usuario
+                $new = GoogleUser::create([
+                    'name' => $googleUser->name,
+                    'email' => $googleUser->email,
+                    'google_id' => $googleUser->id,
+                    'password' => bcrypt('password') // Genera una contraseña predeterminada o usa una lógica diferente
+                ]);
+
+                // Inicia sesión con el nuevo usuario
+                Auth::login($new);
+            }
+
+            return redirect()->route('home'); // Redirige a la ruta deseada
+
+        } catch (\Exception $e) {
+            return redirect('/')->withErrors('Error en la autenticación con Google: ' . $e->getMessage());
+        }
+    }
 }
--- back/routes/api.php
+++ back/routes/api.php
@@ -2,15 +2,25 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\GoogleController;
+use App\Http\Controllers\Auth\GoogleController;
+use AWS\CRT\HTTP\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use SebastianBergmann\CodeCoverage\Driver\Driver;
 
 //users
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 //users_google
-Route::get('auth/google', [GoogleController::class, 'redirectToGoogle']);
-Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
+Route::get('auth/google', function(Request $request){
+    return Socialite::driver('google')->redirect();
+});
+Route::get('auth/google/callback', function (Request $request){
+    dd(Socialite::driver('google')->user());
+});
 
 // rutas privadas
 Route::middleware('auth:api')->group(function () {
