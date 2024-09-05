af5b5d5287ade7710bb025508d1d10856961b67f
Carlos <fajardobeto72@gmail.com>
2024-09-04 20:52:32 -0500

    feat de login and logout

--- back/routes/api.php
+++ back/routes/api.php
@@ -12,15 +12,7 @@ use SebastianBergmann\CodeCoverage\Driver\Driver;
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 //users_google
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
-Route::get('auth/google', function(Request $request){
-    return Socialite::driver('google')->redirect();
-});
-Route::get('auth/google/callback', function (Request $request){
-    dd(Socialite::driver('google')->user());
-});
+
 
 // rutas privadas
 Route::middleware('auth:api')->group(function () {
