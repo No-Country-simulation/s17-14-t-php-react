9a2b171789820ac1c9439bf217a45f1ad81e8aca
Carlos <fajardobeto72@gmail.com>
2024-09-05 10:37:24 -0500

    feat de mentor get

--- back/routes/api.php
+++ back/routes/api.php
@@ -2,7 +2,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\Auth\GoogleController;
+use App\Http\Controllers\MentorController;
 use AWS\CRT\HTTP\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
@@ -11,8 +11,9 @@ use SebastianBergmann\CodeCoverage\Driver\Driver;
 //users
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
-//users_google
-
+//mentor
+Route::get('mentor', [MentorController::class, 'index']);
+Route::get('mentor/{id}', [MentorController::class, 'show']);
 
 // rutas privadas
 Route::middleware('auth:api')->group(function () {
