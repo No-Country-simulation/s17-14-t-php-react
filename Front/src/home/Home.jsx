8551fb3122c623cc43ea0f6f49c70a380e4465fb
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-09-06 17:21:26 -0300

    feat: create MentorProgess component

--- Front/src/home/Home.jsx
+++ Front/src/home/Home.jsx
@@ -3,12 +3,16 @@ import Hero from "./components/hero/Hero";
 import ListMentors from "./components/ListMentors/ListMentors";
 import CarouselMentors from "./components/CarouselMentors/CarouselMentors";
 import FAQItemContainer from "./components/faqs/FAQItemContainer";
+import MentorProgress from "./mentorprogress/MentorProgress";
 
 export default function Home() {
   return (
     <section className="">
       <section className="-ml-[0.5%]">
         <Hero />
+      </section>
+      <section>
+        <MentorProgress />
       </section>
       <section className="">
         <Categories />
