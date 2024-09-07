f3e081476aec4d5720d97dfe13ef854356bb4cad
Nahuehue <nahu.ro.herrera.2001@gmail.com>
2024-09-07 01:03:00 -0300

    Fix: refactor code

--- back/app/Models/Mentor.php
+++ back/app/Models/Mentor.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
-class Mentor extends Model
 
+class Mentor extends Model
+{
     use HasFactory, Notifiable;
 
     protected $fillable = [
